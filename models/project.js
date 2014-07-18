25c13a3dad6c765a065071734efea614eddac39f
Hemanth Kona <hemanth0690@gmail.com>
2014-07-18 19:59:43 -0400

    models

---
+++ models/project.js
@@ -0,0 +1,59 @@
+/*
+  project.js
+  Project databse schema
+
+  Revision history,
+  Hemanth Kona, 07.18.2014: created
+
+*/
+
+var mongoose = require('mongoose');
+
+// Project collection schema
+var projectSchema = new mongoose.Schema({
+  name:  String, 
+  createdBy: String,
+  createdOn: {type: Date, default: Date.now},
+  data: {
+    design: {
+      lineVoltage: Number,
+      impedanceOne: Number,
+      impedanceTwo: Number,
+      impedanceThree: Number,
+      
+      decrementFactor: Number,
+      growthFactor: Number,
+      physicalGridCoefficient: Number,
+      irregularityFactor: Number,
+      
+      averageResistivity: Number,
+      immediateResistivity: Number,
+      clearingTime: Number,
+      substationLength: Number,
+      substationWidth: Number,
+      widthSpacing: Number,
+      lengthSpacing: Number,
+      earthRodLength: Number,
+      geometricSpacingFactor: Number
+    },
+    construction: {
+      estimatedFaultCurrent: Number,
+      designFaultCurrent: Number,
+      conductorLength: Number,
+      earthMatResistance: Number,
+      gridConductorLength: Number,
+      minEarthRodsNumber: Number,
+      increasedEarthRodsNumber: Number,
+      newGridConductorLength: Number,
+      totalLengthOfCopper: Number,
+      maxStepVoltage: Number,
+      tolerableStepVoltage: Number,
+      maxGridPotentialRise: Number,
+      designGrade: String,
+      recommendation: String,
+      comments: String,
+    }     
+  }
+});
+
+var Project = mongoose.model('Project', projectSchema);
