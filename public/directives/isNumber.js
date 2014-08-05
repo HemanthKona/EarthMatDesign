47c905d2ed5b393fc0969fc218e2573711c7e0f7
Hemanth Kona <hemanth0690@gmail.com>
2014-08-05 04:13:53 -0400

    numbersonlyinput

--- public/directives/isNumber.js
+++ public/directives/isNumber.js
@@ -6,6 +6,27 @@
     Pranav Maharaj, 2014.06.20: created
  */
 
+app.directive('numberOnlyInput', function () {
+    return {
+        restrict: 'EA',
+        template: '<input class="form-control" name="{{inputName}}" ng-model="inputValue" />',
+        scope: {
+            inputValue: '=',
+            inputName: '='
+        },
+        link: function (scope) {
+            scope.$watch('inputValue', function(newValue,oldValue) {
+                var arr = String(newValue).split("");
+                if (arr.length === 0) return;
+                if (arr.length === 1 && (arr[0] == '-' || arr[0] === '.' )) return;
+                if (arr.length === 2 && newValue === '-.') return;
+                if (isNaN(newValue)) {
+                    scope.inputValue = oldValue;
+                }
+            });
+        }
+    };
+});
 
 app.directive('isNumber', function () {
 	return {
@@ -22,4 +43,25 @@ app.directive('isNumber', function () {
             });
 		}
 	};
+});
+
+app.directive('numbersOnly', function(){
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+       modelCtrl.$parsers.push(function (inputValue) {
+           // this next if is necessary for when using ng-required on your input. 
+           // In such cases, when a letter is typed first, this parser will be called
+           // again, and the 2nd time, the value will be undefined
+           if (inputValue == undefined) return '' 
+           var transformedInput = inputValue.replace(/[^0-9]/g, ''); 
+           if (transformedInput!=inputValue) {
+              modelCtrl.$setViewValue(transformedInput);
+              modelCtrl.$render();
+           }         
+
+           return transformedInput;         
+       });
+     }
+   };
 });
