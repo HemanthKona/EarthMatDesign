64d24af891d9837bdd7987eff29b9ce1096bd385
Hemanth Kona <hemanth0690@gmail.com>
2014-08-07 14:01:56 -0400

    image updated

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -6,10 +6,38 @@
 	Pranav Maharaj, 2014.06.20: created
 */
 
-app.controller('LoginController', [ '$scope', 'Auth',  
-	function($scope, Auth) {
+app.controller('LoginController', [ '$scope', 'Auth', '$modal', 
+	function($scope, Auth, $modal) {
 		$scope.email = "Hello"
+
 		$scope.login = function() {
+			
+			$scope.formErrors = [];
+
+ 		if($scope.email == undefined) {
+ 			$scope.formErrors.push('Email');
+ 		}
+
+ 		if($scope.password == undefined) {
+ 			$scope.formErrors.push("Password");
+ 		}
+
+ 		if($scope.formErrors.length != 0) {
+				
+				// Concatenating all the data in the array to form a string, that string is sent to content in $modal
+				$scope.AllErrors = $scope.formErrors.join(', <br>')
+
+				// Pop up or Modal to show the error list
+				$modal({
+					title: "Error: Missing Input Data",
+					backdrop: false,
+					html: true,
+					content: $scope.AllErrors
+				});
+
+				return;
+		}
+
 			Auth.login({
 				email: $scope.email,
 				password: $scope.password
--- public/controllers/signup.js
+++ public/controllers/signup.js
@@ -6,10 +6,43 @@
 	Pranav Maharaj, 2014.06.20: created
  */
 
- app.controller('SignupController', [ '$scope', 'Auth', '$alert', 
- 	function($scope, Auth, $alert) {
+ app.controller('SignupController', [ '$scope', 'Auth', '$alert', '$modal', 
+ 	function($scope, Auth, $alert, $modal) {
 	 	$scope.signup = function() {
 	 		
+	 		$scope.formErrors = [];
+
+	 		if($scope.firstname == undefined) {
+	 			$scope.formErrors.push('Firstname');
+	 		}
+
+	 		if($scope.lastname == undefined) {
+	 			$scope.formErrors.push('Lastname');
+	 		}
+
+	 		if($scope.email == undefined) {
+	 			$scope.formErrors.push('Email');
+	 		}
+
+	 		if($scope.password == undefined) {
+	 			$scope.formErrors.push('password');
+	 		}
+
+	 		if($scope.formErrors.length != 0) {
+				
+				// Concatenating all the data in the array to form a string, that string is sent to content in $modal
+				$scope.AllErrors = $scope.formErrors.join(', <br>')
+
+				// Pop up or Modal to show the error list
+				$modal({
+					title: "Error: Missing Input Data",
+					backdrop: false,
+					html: true,
+					content: $scope.AllErrors
+				});
+
+				return;
+			}
 	 		
 	 		Auth.signup({
 	 			firstname: $scope.firstname,
